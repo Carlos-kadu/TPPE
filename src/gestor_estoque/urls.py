d74fa523725a0847af612a833b40bd6f90e07167
Carlos Eduardo Rodrigues <eduardokadu831@gmail.com>
2025-06-10 16:55:09 -0300

    feat(urls): Adiciona rota da API para os produtos

--- src/gestor_estoque/urls.py
+++ src/gestor_estoque/urls.py
@@ -15,8 +15,12 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from gestor_estoque.swagger import schema_view
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('api/', include('produtos.urls')),
+
+    path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
 ]
