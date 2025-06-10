ee622248883820da20e48a54387230fe12b00fe0
Carlos Eduardo Rodrigues <eduardokadu831@gmail.com>
2025-06-10 16:18:22 -0300

    feat(models): Adiciona models da filial

--- src/filial/models.py
+++ src/filial/models.py
@@ -1,3 +1,15 @@
 from django.db import models
+from empresa.models import Empresa
 
-# Create your models here.
+class Filial(models.Model):
+    id_filial = models.AutoField(primary_key=True)
+
+    nome_cidade = models.CharField(max_length=100)
+    empresa = models.ForeignKey(Empresa, on_delete=models.CASCADE, related_name='filiais', db_column='id_empresa')
+    qtd_produtos = models.IntegerField(default=0)
+
+    class Meta:
+        db_table = 'filial'
+
+    def __str__(self):
+        return self.nome_cidade
