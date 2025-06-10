1e5274ff71d45e5ebc141fd78d73f9e252c2428a
Carlos Eduardo Rodrigues <eduardokadu831@gmail.com>
2025-06-10 16:17:37 -0300

    feat(models): Adiciona models da empresa

--- src/empresa/models.py
+++ src/empresa/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
-# Create your models here.
+class Empresa(models.Model):
+    id_empresa = models.AutoField(primary_key=True)
+    razao_social = models.CharField(max_length=100)
+    cnpj = models.CharField(max_length=18, unique=True)
+    qtd_filiais = models.IntegerField(default=0)
+    num_max_filiais = models.IntegerField()
+
+    class Meta:
+        db_table = 'empresa'
+
+    def __str__(self):
+        return self.razao_social
