d74b297f47a6e2a107df9ec0f1f100c928b34263
Carlos Eduardo Rodrigues <eduardokadu831@gmail.com>
2025-06-10 16:18:45 -0300

    feat(models): Adiciona models dos produtos

--- src/produtos/models.py
+++ src/produtos/models.py
@@ -1,3 +1,21 @@
 from django.db import models
+from filial.models import Filial
 
-# Create your models here.
+class Produto(models.Model):
+    indice = models.AutoField(primary_key=True)
+    nome = models.CharField(max_length=100)
+    preco = models.DecimalField(max_digits=10, decimal_places=2)
+    quant = models.IntegerField()
+    id_filial = models.ForeignKey(
+        Filial,
+        on_delete=models.CASCADE,
+        db_column='id_filial'
+    )
+    descricao = models.TextField(blank=True, null=True)
+    tipo = models.CharField(max_length=50)
+
+    class Meta:
+        db_table = 'produto'
+
+    def __str__(self):
+        return f"{self.nome} ({self.tipo})"
