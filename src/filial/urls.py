f7665c7ef35bd47114081c9030a70e6401ada27d
Carlos Eduardo Rodrigues <eduardokadu831@gmail.com>
2025-06-11 15:35:16 -0300

    feat(urls): Adiciona arquivo de urls para o app filial

---
+++ src/filial/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
+from .views import FilialViewSet
+
+router = DefaultRouter()
+router.register(r'filiais', FilialViewSet)
+
+urlpatterns = [
+    path('', include(router.urls)),
+]
