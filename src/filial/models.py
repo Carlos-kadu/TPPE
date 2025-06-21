c6ef5102718bd7f9317a5602f98f625072966d62
Carlos Eduardo Rodrigues <eduardokadu831@gmail.com>
2025-06-21 16:48:01 -0300

    feat(models): Altera modelo do app filial

--- src/filial/models.py
+++ src/filial/models.py
@@ -13,3 +13,29 @@ class Filial(models.Model):
 
     def __str__(self):
         return self.nome_cidade
+
+    def adc_item(self, produto):
+        produto.filial = self
+        produto.save()
+        self.qtd_produtos = self.produtos.count()
+        self.save()
+
+    def editar_item(self, produto, quantidade):
+        produto.quant = quantidade
+        produto.save()
+
+    def excluir_item(self, indice):
+        produto = self.produtos.filter(indice=indice).first()
+        if produto:
+            produto.delete()
+            self.qtd_produtos = self.produtos.count()
+            self.save()
+
+    def buscar_item(self, nome):
+        return self.produtos.filter(nome__iexact=nome).first()
+
+    def obter_caracteristicas_principais(self):
+        return [
+            [p.nome, str(p.preco), str(p.quant), self.nome_cidade, p.__class__.__name__]
+            for p in self.produtos.all()
+        ]
