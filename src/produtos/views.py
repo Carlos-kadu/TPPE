8f3c8fd63496d543caee1fcca7127cbecde1b186
Carlos Eduardo Rodrigues <eduardokadu831@gmail.com>
2025-06-22 10:46:25 -0300

    feat(produtos): Correção nas views para refletir as mudanças no app produtos

--- src/produtos/views.py
+++ src/produtos/views.py
@@ -1,9 +1,16 @@
 from django.shortcuts import render
-
 from rest_framework import viewsets
-from .models import Produto
-from .serializers import ProdutoSerializer
+from .models import Alimentacao, Vestuario, UtilidadesDomesticas
+from .serializers import AlimentacaoSerializer, VestuarioSerializer, UtilidadesDomesticasSerializer
 
-class ProdutoViewSet(viewsets.ModelViewSet):
-    queryset = Produto.objects.all()
-    serializer_class = ProdutoSerializer
+class AlimentacaoViewSet(viewsets.ModelViewSet):
+    queryset = Alimentacao.objects.all()
+    serializer_class = AlimentacaoSerializer
+
+class VestuarioViewSet(viewsets.ModelViewSet):
+    queryset = Vestuario.objects.all()
+    serializer_class = VestuarioSerializer
+
+class UtilidadesDomesticasViewSet(viewsets.ModelViewSet):
+    queryset = UtilidadesDomesticas.objects.all()
+    serializer_class = UtilidadesDomesticasSerializer
