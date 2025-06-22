eff19eb90638f56f10e18a974ed6c6bd432cc1e3
Carlos Eduardo Rodrigues <eduardokadu831@gmail.com>
2025-06-22 10:38:15 -0300

    feat(produtos): altera modelo do app produto

--- src/produtos/models.py
+++ src/produtos/models.py
@@ -6,16 +6,59 @@ class Produto(models.Model):
     nome = models.CharField(max_length=100)
     preco = models.DecimalField(max_digits=10, decimal_places=2)
     quant = models.IntegerField()
-    id_filial = models.ForeignKey(
-        Filial,
-        on_delete=models.CASCADE,
-        db_column='id_filial'
-    )
     descricao = models.TextField(blank=True, null=True)
-    tipo = models.CharField(max_length=50)
 
     class Meta:
         db_table = 'produto'
+        abstract = True
 
     def __str__(self):
-        return f"{self.nome} ({self.tipo})"
+        return f"{self.nome} ({self.__class__.__name__})"
+
+    def editar_caracteristica(self, nome, preco, quant, filial, descricao):
+        self.nome = nome
+        self.preco = preco
+        self.quant = quant
+        self.filial = filial
+        self.descricao = descricao
+        self.save()
+
+class Alimentacao(Produto):
+    filial = models.ForeignKey(
+        Filial,
+        on_delete=models.CASCADE,
+        related_name='alimentacoes',
+        db_column='id_filial'
+    )
+    peso = models.FloatField()
+    vegetariano = models.BooleanField()
+
+    class Meta:
+        db_table = 'alimentacao'
+
+class Vestuario(Produto):
+    filial = models.ForeignKey(
+        Filial,
+        on_delete=models.CASCADE,
+        related_name='vestuarios',
+        db_column='id_filial'
+    )
+    tamanho = models.IntegerField()
+    genero = models.CharField(max_length=20)
+
+    class Meta:
+        db_table = 'vestuario'
+
+class UtilidadesDomesticas(Produto):
+    filial = models.ForeignKey(
+        Filial,
+        on_delete=models.CASCADE,
+        related_name='utilidades_domesticas',
+        db_column='id_filial'
+    )
+    material = models.CharField(max_length=50)
+    marca = models.CharField(max_length=50)
+    caracteristicas = models.TextField()
+
+    class Meta:
+        db_table = 'utilidades_domesticas'
