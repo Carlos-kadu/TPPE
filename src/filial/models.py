4a4ad17a1a881240011d5aec854130c87eba9629
Carlos Eduardo Rodrigues <eduardokadu831@gmail.com>
2025-06-24 20:51:26 -0300

    feat(filial): remove atributo derivado do banco

--- src/filial/models.py
+++ src/filial/models.py
@@ -6,7 +6,6 @@ class Filial(models.Model):
 
     nome_cidade = models.CharField(max_length=100)
     empresa = models.ForeignKey(Empresa, on_delete=models.CASCADE, related_name='filiais', db_column='id_empresa')
-    qtd_produtos = models.IntegerField(default=0)
 
     class Meta:
         db_table = 'filial'
@@ -14,11 +13,13 @@ class Filial(models.Model):
     def __str__(self):
         return self.nome_cidade
 
+    @property
+    def qtd_produtos(self):
+        return self.produtos.count()
+
     def adc_item(self, produto):
         produto.filial = self
         produto.save()
-        self.qtd_produtos = self.produtos.count()
-        self.save()
 
     def editar_item(self, produto, quantidade):
         produto.quant = quantidade
@@ -28,8 +29,6 @@ class Filial(models.Model):
         produto = self.produtos.filter(indice=indice).first()
         if produto:
             produto.delete()
-            self.qtd_produtos = self.produtos.count()
-            self.save()
 
     def buscar_item(self, nome):
         return self.produtos.filter(nome__iexact=nome).first()
