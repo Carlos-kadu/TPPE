0a857ee568df3e0ca59b6aee15638dc959271699
Carlos Eduardo Rodrigues <eduardokadu831@gmail.com>
2025-06-30 19:38:45 -0300

    refactor(empresa): ajusta formatação e adequação ao flake8

--- src/empresa/views.py
+++ src/empresa/views.py
@@ -1,7 +1,7 @@
-from django.shortcuts import render
 from rest_framework import viewsets
 from .models import Empresa
 from .serializers import EmpresaSerializer
+
 
 class EmpresaViewSet(viewsets.ModelViewSet):
     queryset = Empresa.objects.all()
