14d0ae92414f0173df69b824016580fcc6ec1ac9
Carlos Eduardo Rodrigues <eduardokadu831@gmail.com>
2025-06-30 19:37:10 -0300

    refactor(empresa): ajusta formatação e adequação ao flake8

--- src/empresa/serializers.py
+++ src/empresa/serializers.py
@@ -1,7 +1,8 @@
 from rest_framework import serializers
 from .models import Empresa
 
+
 class EmpresaSerializer(serializers.ModelSerializer):
     class Meta:
         model = Empresa
-        fields = '__all__' 
+        fields = '__all__'
