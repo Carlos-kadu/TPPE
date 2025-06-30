c877982e89a6f83f56574d13a929448266375fb6
Carlos Eduardo Rodrigues <eduardokadu831@gmail.com>
2025-06-30 16:12:43 -0300

    feat(empresa): adiciona métodos e propriedade qtd_filiais

--- src/empresa/models.py
+++ src/empresa/models.py
@@ -1,10 +1,10 @@
 from django.db import models
 
+
 class Empresa(models.Model):
     id_empresa = models.AutoField(primary_key=True)
-    razao_social = models.CharField(max_length=100)
+    razao_social = models.CharField(max_length=100, unique=True)
     cnpj = models.CharField(max_length=18, unique=True)
-    qtd_filiais = models.IntegerField(default=0)
     num_max_filiais = models.IntegerField()
 
     class Meta:
@@ -13,17 +13,35 @@ class Empresa(models.Model):
     def __str__(self):
         return self.razao_social
 
+    @property
+    def qtd_filiais(self):
+        return self.filiais.count()
+
     def adc_filial(self, filial):
         if self.filiais.count() < self.num_max_filiais:
             filial.empresa = self
             filial.save()
-            self.qtd_filiais = self.filiais.count()
-            self.save()
 
     def excluir_filial(self, filial):
         filial.delete()
-        self.qtd_filiais = self.filiais.count()
-        self.save()
 
     def buscar_filial(self, nome):
         return self.filiais.filter(nome_cidade__iexact=nome).first()
+
+    def editar_empresa(self, **kwargs):
+        for attr, value in kwargs.items():
+            if hasattr(self, attr):
+                setattr(self, attr, value)
+        self.save()
+
+    def visualizar_empresa(self):
+        return {
+            'id_empresa': self.id_empresa,
+            'razao_social': self.razao_social,
+            'cnpj': self.cnpj,
+            'num_max_filiais': self.num_max_filiais,
+            'qtd_filiais': self.qtd_filiais
+        }
+
+    def deletar_empresa(self):
+        self.delete()
