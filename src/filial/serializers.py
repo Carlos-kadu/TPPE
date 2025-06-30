0475c6efbc515f2c371a2f90159265d325cff255
Carlos Eduardo Rodrigues <eduardokadu831@gmail.com>
2025-06-30 19:39:45 -0300

    refactor(filial): ajusta formatação e adequação ao flake8

--- src/filial/serializers.py
+++ src/filial/serializers.py
@@ -1,7 +1,8 @@
 from rest_framework import serializers
 from .models import Filial
 
+
 class FilialSerializer(serializers.ModelSerializer):
     class Meta:
         model = Filial
-        fields = '__all__' 
+        fields = '__all__'
